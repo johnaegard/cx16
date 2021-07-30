7a5d3cb685d4103d7130ff803b7ec02a0139254a
johnzo <github@johnzo.com>
2021-07-30 04:37:22 -0700

    tiles and very crude map loading into memory, next randomize tiles

--- src/bosconian.asm
+++ src/bosconian.asm
@@ -8,29 +8,40 @@
 
    jmp start
 
-message: .byte "hello, world!"
-end_msg:
-
-NEWLINE = $0D
-UPPERCASE = $8E
-DOUBLE_WIDE_PIXELS = $40
-
 ; VRAM Addresses
-VRAM_layer0_map   = $00000
-VRAM_layer1_map   = $00200
-VRAM_tiles        = $00800
+; https://docs.google.com/spreadsheets/d/1n0DPc4DzMAWshT9GZvgzJAs2BIdy6EfK9pPbRWDD-3A/edit?usp=sharing
 
-VERA_mode_just_layer0_enabled = %00010001
+VRAM_tiles        = $00000
+VRAM_layer0_map   = $00800
+VRAM_layer1_map   = $08800
 
-; 128 x 128 tile map
-; 16 color mode
-; 1bpp 
-VERA_tile_layer0_config = %10100001
+;
+; VERA CONFIGS
+;
+; 320x200
+VERA_pixel_scale = $40
+; just layer 0 enabled
+VERA_mode = %00010001
+; 128 x 128 tile map, 16 color mode, 1bpp
+VERA_tile_layer0_config = %10100000 
+
+tiles:
+.include "tiles.inc"
+end_tiles:
+TILES_SIZE = end_tiles-tiles
+
+layer_0_map:
+.byte $00,$00,$01,$01,$02,$02,$03,$03
+.byte $04,$04,$05,$05,$06,$06,$07,$07
+.byte $08,$08,$09,$09,$0A,$0A,$0B,$0B
+.byte $0C,$0C,$0D,$0D,$0E,$0E,$0F,$0F
+end_layer_0_map:
+L0_MAP_SIZE = end_layer_0_map - layer_0_map
 
 start:
 
-   ; set resolution to 320x240
-   lda #DOUBLE_WIDE_PIXELS
+   ; resolution
+   lda #VERA_pixel_scale
    sta VERA_dc_hscale
    sta VERA_dc_vscale
 
@@ -49,39 +60,21 @@ start:
    lda #(VRAM_tiles >> 9) 
    sta VERA_L0_tilebase
 
-   stz VERA_L0_hscroll_l ; horizontal scroll = 0
-   stz VERA_L0_hscroll_h
-   stz VERA_L0_vscroll_l ; vertical scroll = 0
-   stz VERA_L0_vscroll_h
+   ;; reset scroll
+   ; stz VERA_L0_hscroll_l ; horizontal scroll = 0
+   ; stz VERA_L0_hscroll_h
+   ; stz VERA_L0_vscroll_l ; vertical scroll = 0
+   ; stz VERA_L0_vscroll_h
 
    ; reenable display
-   lda #VERA_mode_just_layer0_enabled
+   lda #VERA_mode
    sta VERA_dc_video
-   
-   ; force uppercase
-   lda #UPPERCASE
-   jsr CHROUT
-   ; print message
-   lda #<message
-   sta ZP_PTR_1
-   lda #>message
-   sta ZP_PTR_1+1
-   ldy #0
-@loop:
-   cpy #(end_msg-message)
-   beq @done
-   lda (ZP_PTR_1),y
-   jsr CHROUT
-   iny
-   bra @loop
+
+   ; load tile definitions to VRAM
+   RAM2VRAM tiles, VRAM_tiles, TILES_SIZE
+
+   ; load layer 0 map to VRAM
+   RAM2VRAM layer_0_map, VRAM_layer0_map, L0_MAP_SIZE
+
 @done:
-   ; print newline
-   lda #NEWLINE
-   jsr CHROUT
    rts
-
-
-   lda $40
-   sta VERA_dc_hscale
-   sta VERA_dc_vscale
-
